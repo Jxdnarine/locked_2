02199b5e7361a236fd661512758fbed3a530aec9
Adrian Judnarine <ajudnarine@gmail.com>
2024-01-10 14:40:33 -0500

    This is an amazing website

---
+++ android/app/src/main/kotlin/com/example/locked/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.locked
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/consts.dart
@@ -0,0 +1,1 @@
+const OPENAI_API_KEY = "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";
